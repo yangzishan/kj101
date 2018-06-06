fda98c3e9c7e98586080cf891f71e01cd15b955d
yangzishan <yangzishan@jiankangzhan.com>
2018-06-06 10:24:23 +0800

    添加素问 528

--- 38_sw/js/base.js
+++ 38_sw/js/base.js
@@ -9,5 +9,4 @@ var shareImgUrl = 'http://image.jiankangzhan.com'; //分享图片
 /*var dataUrl = 'http://sw.jiankangzhan.com'; //数据接口
 var testHealthUrl = 'http://kj101.jiankangzhan.com'; //大机器
 var bit_testHealthUrl = 'http://kj201.jiankangzhan.com'; //小机器
-var shareImgUrl = 'http://image.jiankangzhan.com'; //分享图片
-*/
+var shareImgUrl = 'http://image.jiankangzhan.com'; //分享图片*/
